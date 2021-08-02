a2ca9d66185cf20bd0589864be31f888e74c50ca
Zavina Andrey <zavinaag@gmail.com>
2021-08-03 00:12:39 +0300

    wtf refactoring

--- user_login_form.py
+++ user_login_form.py
@@ -1,13 +1,10 @@
-from flask_wtf import FlaskForm
-from wtforms import TextField, SubmitField
-from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import TextField, SubmitField, PasswordField, StringField
+from wtforms.validators import DataRequired, Length, InputRequired
 
 
-class Login_form(FlaskForm):
-    user = TextField(validators=[DataRequired()])
-    password = TextField()
+class LoginForm(FlaskForm):
+    user = StringField(validators=[InputRequired(), Length(min=5, max=20, message='Must Be 5-10 characters')])
+    password = PasswordField()
+    rec = RecaptchaField()
     submit = SubmitField('LogIn')
-
-
-
-
--- web_count_letter.py
+++ web_count_letter.py
@@ -3,16 +3,20 @@ from datetime import datetime
 from count_letter import ccount
 from dbinterface import log_event_ins, log_event_get, log_event_ins_dbcm
 from m_checker import check_logged_in
-from user_login_form import Login_form
+from user_login_form import LoginForm
 
 app = Flask(__name__)
 
 now = datetime.now()
 logging_meta = dict()
 
-app.secret_key = '30 82 01 0a 02 82 01 01 00 df 90 d2 ae 90 5a 80 11 c1 df 78 75 9a f9 ee 3c a7 54 44 bb a7 8e 2b 92 29 ef dc e5 0a 57 3b 31 07 6c 1d 14 f5 fe d3 ef 25 9c fe c8 8a e0 0c b2 e7 78 61 f8 5d 1f 0e 87 d8 7f 06 f5 4a ec df da 82 da 1f d8 99 ed 26 be d0 55 79 19 bd 88 69 33 f4 62 4d 84 65 af 4d cb ac 8d e4 37 45 a6 de 1c 94 99 1e b7 eb b5 ab 96 76 af b2 38 08 f9 55 f0 13 a1 fa d3 5a af df ff 0a 2d e7 d7 f2 cd 4c bc f5 d6 ff 8e 0f 87 f1 55 c6 92 51 cf 4d 35 81 86 0f 9a 22 a0 76 0b 54 7e 97 8b 22 28 4e e7 53 ff ec 23 08 2d c9 29 1e 3c 37 f4 7b 6e 03 cc c8 95 c0 3e e1 29 24 71 1e 7a 10 a3 4b e6 c6 63 d0 ae 44 18 8d 82 8f 5f 91 1a ae 8a fe 70 22 b7 5a 1e 1f b8 a6 e5 08 00 25 68 97 7c 73 16 c7 86 f1 18 81 1b 5a ee c8 96 a6 64 a4 58 9b 0b 0d a2 53 9d bd f1 d8 ff 4b c3 be 3b 6d 68 5b 45 c2 19 c5 39 02 03 01 00 01'
-
+app.secret_key = """ 01 0a 02 82 01 01 00 df 90 d2 ae 90 5a 80 11 c1 df 78 75 9a f9 ee 3c a7 54 44 bb a7 8e 2b 92
+                 q9 ef dc e5 0a 57 3b 31 07 6c 1d 14 f5 fe d3 ef 25 9c fe c8 8a e0 0c b2 e7 78 61 f8 5d 1f 0e 87 d8 7f
+                 06 f5 4a ec df da 82 da 1f d8 99 ed 26 be d0 55 79 19 bd 88 69 33 f4 62 4d 84 65 af 4d cb ac 8d e4 37
+                 45 a6 de 1c 94 99 1e b7 eb b5 ab 96 76 af b2 38 08 f9 55 f0 13 a1 fa d3 5a af df ff 0a 2d e7 d7 f2"""
 app.config['dbconfig'] = {'database': 'webapp.db'}
+app.config['RECAPTCHA_PUBLIC_KEY'] = "6LeMhdUbAAAAAM0CcNXeUCPpFjklNupdOOsI9OHH"
+app.config['RECAPTCHA_PRIVATE_KEY'] = "6LeMhdUbAAAAAENm-E3rOPTen9Q32t-RgJyh4zal"
 
 
 def get_datetimen() -> str:
@@ -34,7 +38,7 @@ def log_request_dbcm(req, res) -> None:
                        app.config['dbconfig'])
 
 
-def set_session(user: str = 'Anonimous', password: str = 'pass', logged_in: bool = True) -> None:
+def set_session(user: str = 'Anonimous', password: str = 'pass', logged_in: bool = True):
     session['logged_in'] = logged_in
     session['password'] = password
     session['user'] = user
@@ -57,7 +61,7 @@ def index_page():
 
 @app.route('/countthis', methods=['POST'])
 @check_logged_in
-def web_count() -> str:
+def web_count():
     try:
         if request.form['PhraseInput'].strip() != '':
             the_result = ccount(request.form['PhraseInput'])
@@ -80,7 +84,7 @@ def web_count() -> str:
 
 @app.route('/viewlog')
 @check_logged_in
-def get_log() -> 'html':
+def get_log():
     try:
         contents = []
         with open('log.txt') as log:
@@ -95,57 +99,49 @@ def get_log() -> 'html':
 
 @app.route('/viewlogdb')
 @check_logged_in
-def get_log_db() -> 'html':
+def get_log_db():
     res = log_event_get()
     return render_template('log.html', the_title='Log View DB', the_data=res, user=get_session_user())
 
 
 @app.route('/lgf', methods=['GET'])
 def user_login_form():
-    ulf = Login_form()
+    ulf = LoginForm()
     return render_template('user_login_form.html', title="Авторизация пользователя", form=ulf)
 
 
 @app.route('/lgf2', methods=['GET'])
+@app.route('/login2', methods=['POST'])
 def user_login_form_wtf():
-    ulfw = Login_form()
-    return render_template('ulfw.html', form=ulfw)
+    ulfw = LoginForm()
+    if ulfw.validate_on_submit():
+        set_session(password=ulfw.password.data, user=ulfw.user.data)
+        return redirect('/index')
+    else:
+        return render_template('ulfw.html', form=ulfw)
 
 
 @app.route('/login', methods=['POST'])
 def set_session_user():
-    if (not request.form['user']):
+    if not request.form['user']:
         set_session()
     else:
         set_session(password=request.form['password'], user=request.form['user'])
     return redirect('/index')
 
 
-@app.route('/login2', methods=['POST', 'GET'])
-def set_session_user2():
-    ulfw = Login_form()
-    if ulfw.validate_on_submit():
-        if (not ulfw.user.data):
-            return redirect('/lgf2')
-        else:
-            set_session(password=ulfw.password.data, user=ulfw.user.data)
-        return redirect('/index')
-    else:
-        set_session()
-        return redirect('/index')
-
-
 @app.route('/logout', methods=['GET'])
 @check_logged_in
 def logout() -> '302':
-    if 'logged_in' and 'user' in session:
+    if 'logged_in' in session:
         session.pop('logged_in')
         session.pop('user')
+        session.pop('password')
     return redirect('/')
 
 
 @app.route('/accden')
-def accden() -> 'html':
+def accden():
     return render_template("accden.html", the_title='')
 
 
